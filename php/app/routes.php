83df02cf1f6027df8d3286a3f7e77a8edf3bd837
Guilherme Smith <guilhermessmith2014@gmail.com>
2026-03-27 03:20:12 -0300

    feat(routes): adiciona rotas de usuário e posts
    - /auth/logout
    - /user/avatar
    - /post/create

--- php/app/routes.php
+++ php/app/routes.php
@@ -5,4 +5,13 @@ $app->get('/', 'HomeController:index')->setName('home');
 $app->group('/auth',function($app){
     $app->map(['GET','POST'],'/login','AuthController:login')->setName('auth.login');
     $app->map(['GET','POST'],'/registrar','AuthController:register')->setName('auth.register');
+    $app->map(['GET','POST'],'/logout','AuthController:logout')->setName('auth.logout');
+});
+
+$app->group('/user',function($app){
+    $app->map(['GET','POST'],'/avatar','HomeController:avatar')->setName('user.avatar');
+});
+
+$app->group('/post',function($app){
+    $app->map(['GET','POST'],'/create','PostController:create')->setName('post.create');
 });
